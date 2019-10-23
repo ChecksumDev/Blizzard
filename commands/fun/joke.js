951500ce637bb8fa6e060403d69b948beb59d7a3
CollierPlays <collier.bell20@gmail.com>
2019-10-23 10:54:28 -0500

    Update joke.js

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,6 +1,25 @@
-var request = require('request');
-request
-  .get('https://icanhazdadjoke.com/')
-  .set('Accept', 'application/json')
-  .end(function(err, res){
-  });
+require("dotenv").config();
+const commando = require("discord.js-commando");
+const Discord = require("discord.js");
+
+module.exports = class DogCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "dog",
+      group: "fun",
+      memberName: "dog",
+      description: "Woof."
+    });
+  }
+
+  async run(message, args) {
+    const querystring = require("querystring");
+    const query = querystring.stringify();
+
+    const { text } = await fetch(`https://icanhazdadjoke.com/`).then(response =>
+      response.json().then(res => {
+        console.log(res);
+      })
+    );
+  }
+};
