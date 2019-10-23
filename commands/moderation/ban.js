1bda738c1708a3bf7fe6bed826393fb0ff9f6048
CollierPlays <collier.bell20@gmail.com>
2019-10-22 20:36:18 -0500

    Added kick and ban commands

---
+++ commands/moderation/ban.js
@@ -0,0 +1,22 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class BanCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'BAN',
+            group: 'moderation',
+            memberName: 'ban',
+            description: 'Just a ban command :)',
+            clientPermissions: ['BAN_MEMBERS'],
+            userPermissions: ['BAN_MEMBERS'],
+        })
+    }
+
+    async run(msg) {
+        var member = msg.mentions.members.first();
+        member.ban().then((member) => {
+            // Successmessage
+            msg.channel.send(":wave: " + member.displayName + " has been successfully Banned :point_right: ");
+        })
+}
---
+++ commands/moderation/kick.js
@@ -0,0 +1,23 @@
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+
+module.exports = class KickCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'kick',
+            aliases: ["k"],
+            group: 'moderation',
+            memberName: 'kick',
+            description: 'Just a kick command :)',
+            clientPermissions: ['KICK_MEMBERS'],
+            userPermissions: ['KICK_MEMBERS'],
+        })
+    }
+
+    async run(msg) {
+        var member = msg.mentions.members.first();
+        member.kick().then((member) => {
+            // Successmessage
+            msg.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        })
+}
