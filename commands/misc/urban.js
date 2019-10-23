836e791c0be112ffc6d2d172c3e860f81316785e
CollierPlays <collier.bell20@gmail.com>
2019-10-23 11:07:48 -0500

    Create urban.js

---
+++ commands/misc/urban.js
@@ -0,0 +1,23 @@
+		if (!args.length) {
+			return message.channel.send('You need to supply a search term!');
+		}
+
+		const query = querystring.stringify({ term: args.join(' ') });
+
+		const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
+
+		if (!list.length) {
+			return message.channel.send(`No results found for **${args.join(' ')}**.`);
+		}
+
+		const [answer] = list;
+
+		const embed = new Discord.MessageEmbed()
+			.setColor('#EFFF00')
+			.setTitle(answer.word)
+			.setURL(answer.permalink)
+			.addField('Definition', trim(answer.definition, 1024))
+			.addField('Example', trim(answer.example, 1024))
+			.addField('Rating', `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`);
+
+		message.channel.send(embed);
