cc169de3f5a2a0a5a1ba9eec9864713152cd1bb8
CollierPlays <collier.bell20@gmail.com>
2019-10-23 12:35:18 -0500

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,3 +1,20 @@
-let messagecount = parseInt(numberofmessages);
-message.channel.fetchMessages({ limit: messagecount })
+const { Command } = require('discord.js-commando');
+
+module.exports = class PurgeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'purge',
+			group: 'moderation',
+			memberName: 'purge',
+			description: 'Purges the Chat',
+      clientPermissions: ['MANAGE_MESSAGES'],
+      userPermissions: ['MANAGE_MESSAGES'],
+		});
+	}
+
+	run(message, args) {
+		let messagecount = parseInt(args[0]);
+      message.channel.fetchMessages({ limit: messagecount })
   .then(messages => message.channel.bulkDelete(messages));
+	}
+};
