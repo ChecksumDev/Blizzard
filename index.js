0c45d077ecfb290f169f3c340f7609cbcf870717
CollierPlays <collier.bell20@gmail.com>
2019-10-24 09:36:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 require("dotenv").config();
 const { CommandoClient } = require("discord.js-commando");
+const { Discord } = require("discord.js");
 const path = require("path");
-const Keyv = require("keyv")
-const database = new Keyv(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:5432/${process.env.DB_DATABASE}`);
-
+const Keyv = require("keyv");
+const Canvas = require("canvas");
+const database = new Keyv(
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
+    process.env.DB_URL
+  }:5432/${process.env.DB_DATABASE}`
+);
 
 const client = new CommandoClient({
   commandPrefix: "b?",
   unknownCommandResponse: false,
   disableEveryone: false,
   invite: "https://discord.gg/SsT9KT"
+});
+
+const applyText = (canvas, text) => {
+  const ctx = canvas.getContext("2d");
+  let fontSize = 70;
+
+  do {
+    ctx.font = `${(fontSize -= 10)}px sans-serif`;
+  } while (ctx.measureText(text).width > canvas.width - 300);
+  return ctx.font;
+};
+
+client.on("guildMemberAdd", async member => {
+  const channel = await database.get(member.guild.id + "_welcomechannel");
+  if (!channel) return;
+
+  const canvas = Canvas.createCanvas(700, 250);
+  const ctx = canvas.getContext("2d");
+
+  const background = await Canvas.loadImage("./views/wallpaper.jpg");
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "#74037b";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+  // Assign the decided font to the canvas
+  ctx.font = applyText(canvas, member.displayName);
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
+
+  ctx.beginPath();
+  ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+
+  const avatar = await Canvas.loadImage(
+    member.user.displayAvatarURL({ format: "jpg" })
+  );
+  ctx.drawImage(avatar, 25, 25, 200, 200);
+
+  const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+    "welcome-image.png"
+  );
+
+  channel.send(attachment);
 });
 
 client.registry
