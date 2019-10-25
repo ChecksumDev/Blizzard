519d5fa122ba1e6e4119574ce3c8a3633e74a5e4
CollierPlays <collier.bell20@gmail.com>
2019-10-25 04:03:16 -0500

    fixes

--- commands/misc/urban.js
+++ commands/misc/urban.js
@@ -16,7 +16,8 @@ module.exports = class UrbanCommand extends commando.Command {
           prompt: "What would you like to look up?",
           type: "string"
         }
-      ]
+      ],
+      guildOnly: true
     });
   }
 
