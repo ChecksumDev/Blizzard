3985380ab063e3941759d9fb8bad243d07eb316a
CollierPlays <collier.bell20@gmail.com>
2019-10-26 21:36:24 -0500

    Create hug.js

---
+++ commands/fun/hug.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const commando = require("discord.js-commando");
+const oneLine = require("common-tags").oneLine;
+const Discord = require("discord.js");
+const Tenor = require("tenorjs").client({
+  Key: process.env.TENORAPI, // https://tenor.com/developer/keyregistration
+  Filter: "high", // "off", "low", "medium", "high", not case sensitive
+  Locale: "en_US", // Your locale here, case-sensitivity depends on input
+  MediaFilter: "minimal", // either minimal or basic, not case sensitive
+  DateFormat: "D/MM/YYYY - H:mm:ss A" // Change this accordingly
+});
+
+module.exports = class HugCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "hug",
+      aliases: [],
+      group: "fun",
+      memberName: "hug",
+      description: "Hug your friends! <3",
+      details: oneLine`
+                Hug your friends! <3
+            `,
+      examples: ["hug @Wumpus#0000"],
+      throttling: {
+        usages: 2,
+        duration: 10
+      }
+    });
+  }
+
+  async run(msg) {
+    const user = msg.mentions.users.first();
+    await Tenor.Search.Random("anime-hug-gifs", "1")
+      .then(Results => {
+        Results.forEach(Post => {
+          let hugged = new Discord.MessageEmbed()
+            .setTitle(`${user.tag} was furiousley hugged by ${msg.author.tag}`)
+            .setImage(`${Post.url}`);
+          msg.channel.send(hugged);
+        });
+      })
+      .catch(console.error);
+  }
+};
